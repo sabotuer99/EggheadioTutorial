09b47ccbdf1bba83b53c0dbbb43410f9b2f2ee03
troy <sabotuer99@gmail.com>
2015-03-01 20:32:44 -0700

    Twitter example

---
+++ app/twitterApp.js
@@ -0,0 +1,29 @@
+var app = angular.module('twitterApp', []);
+
+app.controller("AppCtrl", function ($scope) {
+    $scope.loadMoreTweets = function () {
+        alert("Loading tweets!");
+    }
+
+    $scope.deleteTweets = function () {
+        alert("deleting tweets!");
+    }
+})
+
+
+app.directive("enter", function () {
+    return function (scope, element, attrs) {
+        element.bind("mouseenter", function () {
+            scope.$apply(attrs.enter);
+        })
+    }
+})
+
+/*
+app.directive("leave", function () {
+    return function (scope, element, attrs) {
+        element.bind("mouseleave", function () {
+            element.removeClass(attrs.enter);
+        })
+    }
+})*/
