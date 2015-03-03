44c7436f9c51a4095723885066fad390123a168d
troy <sabotuer99@gmail.com>
2015-03-02 20:33:31 -0700

    animation with tweenmax

--- app/repeat.js
+++ app/repeat.js
@@ -3,4 +3,15 @@ var app = angular.module("app", ['ngAnimate']);
 app.controller("AppCtrl", function () {
     this.things = ["one", "two", "three"];
     this.toggle = true;
-})
+});
+
+app.animation(".toggle", function() {
+    return {
+        leave: function (element, done) {
+            TweenMax.fromTo(element, 1, {opacity: 1}, {opacity:0, onComplete:done});
+        },
+        enter: function (element, done) {
+            TweenMax.fromTo(element, 1, {opacity: 0}, {opacity:1, onComplete:done});
+        }
+    }
+});
